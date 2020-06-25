964b9568231643972cfed56bb0e9f28628760212
Maria <manya1999m09@yandex.ru>
2020-06-25 23:20:06 +0300

    detection practice

--- modules/3_detection/src/detector.cpp
+++ modules/3_detection/src/detector.cpp
@@ -1,11 +1,13 @@
 #include "detector.hpp"
-
+#include <utility> 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/utils/filesystem.hpp>
 #include <inference_engine.hpp>
+#include <algorithm>
 
 using namespace cv;
 using namespace InferenceEngine;
+using namespace cv::utils::fs;
 
 Detector::Detector() {
     Core ie;
@@ -13,24 +15,146 @@ Detector::Detector() {
     // Load deep learning network into memory
     auto net = ie.ReadNetwork(utils::fs::join(DATA_FOLDER, "face-detection-0104.xml"),
                               utils::fs::join(DATA_FOLDER, "face-detection-0104.bin"));
+	// Specify preprocessing procedures
+	// (NOTE: this part is different for different models!)
+	InputInfo::Ptr inputInfo = net.getInputsInfo()["image"];
+	inputInfo->getPreProcess().setResizeAlgorithm(ResizeAlgorithm::RESIZE_BILINEAR);
+	inputInfo->setLayout(Layout::NHWC);
+	inputInfo->setPrecision(Precision::U8);
+	outputName = net.getOutputsInfo().begin()->first;
+
+	// Initialize runnable object on CPU device
+	ExecutableNetwork execNet = ie.LoadNetwork(net, "CPU");
+
+	// Create a single processing thread
+	req = execNet.CreateInferRequest();
+}
+
+Blob::Ptr wrapMatToBlob(const Mat& m) {
+	CV_Assert(m.depth() == CV_8U);
+	std::vector<size_t> dims = { 1, (size_t)m.channels(), (size_t)m.rows, (size_t)m.cols };
+	return make_shared_blob<uint8_t>(TensorDesc(Precision::U8, dims, Layout::NHWC),
+		m.data);
+}
+
+void Detector::detect(const cv::Mat& image, const float nmsThreshold, 
+					  const float probThreshold, std::vector<cv::Rect>& boxes,
+                      std::vector<float>& probabilities,
+                      std::vector<unsigned>& classes) {
+	// Create 4D blob from BGR image
+	Blob::Ptr input = wrapMatToBlob(image);
+
+	req.SetBlob("image", input);
+
+	// Launch network
+	req.Infer();
+	auto w = image.cols;
+	auto h = image.rows;
+
+	// Copy output. 
+	float* output = req.GetBlob(outputName)->buffer();
+	int size = req.GetBlob(outputName)->size();
+	float score = 0;
+	float cls = 0;
+	float id = 0;
+	for (int i = 0; i < size/7; i++)
+	{
+		score = output[i * 7 + 2];
+		cls = output[i * 7 + 1];
+		id = output[i * 7];
+		if (id >= 0 && score > probThreshold && cls <= 1)
+		{
+			classes.push_back(cls);
+			probabilities.push_back(score);
+			// 3 - xmin, 4 - ymin, 5 - xmax, 6 - ymax
+			int xmin = output[i * 7 + 3] * w;
+			int xmax = output[i * 7 + 5] * w;
+			float ymin = output[i * 7 + 4] * h;
+			float ymax = output[i * 7 + 6] * h;
+			boxes.push_back(Rect(xmin, ymin, int(xmax-xmin) + 1, int(ymax - ymin) + 1));
+		}
+	}
+	std::vector<unsigned> indices;
+	nms(boxes, probabilities, nmsThreshold, indices);
+	//todo
 }
 
 
-void Detector::detect(const cv::Mat& image,
-                      float nmsThreshold,
-                      float probThreshold,
-                      std::vector<cv::Rect>& boxes,
-                      std::vector<float>& probabilities,
-                      std::vector<unsigned>& classes) {
-    CV_Error(Error::StsNotImplemented, "detect");
+
+float IoU(cv::Rect A, cv::Rect B) {
+	float inters = (A & B).area();
+	float uni = A.area() + B.area() - inters;
+	float iou = inters / uni;
+
+	return iou;
 }
 
 
 void nms(const std::vector<cv::Rect>& boxes, const std::vector<float>& probabilities,
-         float threshold, std::vector<unsigned>& indices) {
-    CV_Error(Error::StsNotImplemented, "nms");
-}
+	float threshold, std::vector<unsigned>& indices) {
 
-float iou(const cv::Rect& a, const cv::Rect& b) {
-    CV_Error(Error::StsNotImplemented, "iou");
+	std::vector<cv::Rect> result;
+	std::vector<cv::Rect> b;
+	std::vector<float> prob;
+	copy(boxes.begin(), boxes.end(), back_inserter(b));
+	copy(probabilities.begin(), probabilities.end(), back_inserter(prob));
+
+	while (!b.empty()) {
+		//std::cout << "while has started " << std::endl;
+		auto m = std::max_element(prob.begin(), prob.end()) - prob.begin();
+		//std::cout << "m = " << m << std::endl;
+		cv::Rect M;
+		if (b.size() == 1)
+		{
+			M = b.at(0);
+			b.clear();
+		}
+		else
+		{
+			M = b.at(m);
+			b.erase(b.begin() + m);
+			prob.erase(prob.begin() + m);
+		}
+		
+		std::cout << "M " << M << std::endl;
+
+		result.push_back(M);
+		/*for (size_t i = 0; i < result.size(); i++)
+		{
+			std::cout << "result " << result[i] << std::endl;
+		}
+		
+		
+		for (size_t i = 0; i < b.size(); i++)
+		{
+			std::cout << "left boxes" << b[i] << std::endl;
+		}*/
+
+		for (size_t i = 0; i < b.size(); i++)
+		{
+			//std::cout << "loop begin" <<  std::endl;
+			//std::cout << "iou " << IoU(M, b.at(i)) << std::endl;
+			if (IoU(M, b.at(i)) > threshold)
+			{
+				b.erase(b.begin() + i);
+				prob.erase(prob.begin() + i);
+				/*for (size_t i = 0; i < b.size(); i++)
+				{
+					std::cout << "if worked.left boxes" << b[i] << std::endl;
+				}*/
+			}
+		}
+	}
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		cv::Rect d = result.at(i);
+		auto it = std::find(boxes.begin(), boxes.end(), d);
+		auto index = std::distance(boxes.begin(), it);
+		indices.push_back(index);
+	}
+	for (size_t i = 0; i <probabilities.size(); i++)
+	{
+		std::cout << "probs are " << probabilities[i] << std::endl;
+	}
 }
--- modules/3_detection/test/test_detector.cpp
+++ modules/3_detection/test/test_detector.cpp
@@ -11,90 +11,86 @@ using namespace cv::utils::fs;
 
 // Simple NMS test
 TEST(detection, nms) {
-    std::vector<Rect> boxes = {Rect(24, 17, 75, 129), Rect(38, 38, 88, 134), Rect(30, 88, 150, 105)};
-    std::vector<float> probs = {0.81f, 0.89f, 0.92f};
-    std::vector<unsigned> indices;
+	std::vector<Rect> boxes = { Rect(24, 17, 75, 129), Rect(38, 38, 88, 134), Rect(30, 88, 150, 105) };
+	std::vector<float> probs = { 0.81f, 0.89f, 0.92f };
+	std::vector<unsigned> indices;
 
-    // Replace #if 0 to #if 1 for debug visualization.
+	// Replace #if 0 to #if 1 for debug visualization.
 #if 0
-    Mat img(300, 300, CV_8UC1, Scalar(0));
-    for (int i = 0; i < boxes.size(); ++i) {
-        rectangle(img, boxes[i], 255);
-        putText(img, format("%.2f", probs[i]), Point(boxes[i].x, boxes[i].y - 2),
-                FONT_HERSHEY_SIMPLEX, 0.5, 255);
-    }
-    imshow("nms", img);
-    waitKey();
+	Mat img(300, 300, CV_8UC1, Scalar(0));
+	for (int i = 0; i < boxes.size(); ++i) {
+		rectangle(img, boxes[i], 255);
+		putText(img, format("%.2f", probs[i]), Point(boxes[i].x, boxes[i].y - 2),
+			FONT_HERSHEY_SIMPLEX, 0.5, 255);
+	}
+	imshow("nms", img);
+	waitKey();
 #endif
 
-    nms(boxes, probs, 0.4, indices);
+	nms(boxes, probs, 0.4, indices);
 
-    ASSERT_EQ(indices.size(), 2);
-    ASSERT_EQ(indices[0], 2);
-    ASSERT_EQ(indices[1], 1);
-}
-
-TEST(detection, iou) {
-    Rect a(21, 27, 110, 150);
-    Rect b(53, 56, 108, 152);
-    ASSERT_NEAR(iou(a, b), 0.401993, 1e-5);
+	ASSERT_EQ(indices.size(), 2);
+	ASSERT_EQ(indices[0], 2);
+	ASSERT_EQ(indices[1], 1);
 }
 
 TEST(detection, faces) {
-    const float nmsThreshold = 0.45f;
-    const float probThreshold = 0.3f;
+	const float nmsThreshold = 0.45f;
+	const float probThreshold = 0.3f;
 
-    Mat img = imread(join(DATA_FOLDER, "conference.png"));
-    std::vector<Rect> refBoxes = {
-        Rect(276, 267, 24, 31), Rect(529, 244, 24, 31), Rect(127, 268, 22, 29),
-        Rect(53, 263, 23, 29), Rect(600, 248, 23, 30), Rect(453, 242, 22, 31),
-        Rect(428, 196, 22, 30), Rect(199, 264, 22, 29), Rect(570, 267, 22, 29),
-        Rect(605, 192, 21, 30), Rect(94, 250, 22, 28), Rect(486, 197, 20, 27),
-        Rect(432, 263, 23, 30), Rect(370, 196, 20, 28), Rect(534, 194, 20, 26),
-        Rect(328, 236, 20, 29), Rect(627, 276, 22, 28), Rect(108, 173, 21, 31),
-        Rect(368, 261, 24, 31), Rect(571, 173, 20, 27), Rect(166, 241, 22, 30),
-        Rect(252, 202, 21, 29), Rect(243, 240, 23, 30), Rect(387, 238, 21, 30),
-        Rect(195, 205, 21, 30), Rect(162, 193, 17, 25), Rect(492, 262, 22, 29),
-        Rect(319, 210, 19, 27), Rect(31, 238, 23, 30)
-    };
-    std::vector<float> refProbs = {
-        0.988363, 0.987432, 0.983568, 0.981547, 0.980774, 0.980398,
-        0.97681,  0.972978, 0.964593, 0.960844, 0.958782, 0.942152,
-        0.913904, 0.903534, 0.888043, 0.879184, 0.875234, 0.862958,
-        0.815154, 0.810955, 0.797403, 0.792146, 0.771417, 0.710467,
-        0.696174, 0.619217, 0.577113, 0.418896, 0.306871
-    };
+	Mat img = imread(join(DATA_FOLDER, "conference.png"));
+	std::vector<Rect> refBoxes = {
+		Rect(276, 267, 24, 31), Rect(529, 244, 24, 31), Rect(127, 268, 22, 29),
+		Rect(53, 263, 23, 29), Rect(600, 248, 23, 30), Rect(453, 242, 22, 31),
+		Rect(428, 196, 22, 30), Rect(199, 264, 22, 29), Rect(570, 267, 22, 29),
+		Rect(605, 192, 21, 30), Rect(94, 250, 22, 28), Rect(486, 197, 20, 27),
+		Rect(432, 263, 23, 30), Rect(370, 196, 20, 28), Rect(534, 194, 20, 26),
+		Rect(328, 236, 20, 29), Rect(627, 276, 22, 28), Rect(108, 173, 21, 31),
+		Rect(368, 261, 24, 31), Rect(571, 173, 20, 27), Rect(166, 241, 22, 30),
+		Rect(252, 202, 21, 29), Rect(243, 240, 23, 30), Rect(387, 238, 21, 30),
+		Rect(195, 205, 21, 30), Rect(162, 193, 17, 25), Rect(492, 262, 22, 29),
+		Rect(319, 210, 19, 27), Rect(31, 238, 23, 30)
+	};
+	std::vector<float> refProbs = {
+		0.988363, 0.987432, 0.983568, 0.981547, 0.980774, 0.980398, 0.97681, 
+		0.972978,0.964593, 0.960844, 0.958782, 0.942152, 0.913904, 0.903534,
+		0.888043, 0.879184, 0.875234, 0.862958, 0.815154, 0.810955, 0.797403,
+		0.792146, 0.771417, 0.710467, 0.696174, 0.619217, 0.577113, 0.418896, 0.306871
+	};
 
-    Detector model;
-    std::vector<Rect> boxes;
-    std::vector<float> probs;
-    std::vector<unsigned> classes;
-    model.detect(img, nmsThreshold, probThreshold, boxes, probs, classes);
+	Detector model;
+	std::vector<Rect> boxes;
+	std::vector<float> probs;
+	std::vector<unsigned> classes;
+	model.detect(img, nmsThreshold, probThreshold, boxes, probs, classes);
 
-    // Replace #if 0 to #if 1 for debug visualization.
+	// Replace #if 0 to #if 1 for debug visualization.
 #if 0
-    for (int i = 0; i < boxes.size(); ++i) {
-        rectangle(img, boxes[i], Scalar(0, 0, 255));
-        rectangle(img, refBoxes[i], Scalar(0, 255, 0));
-        putText(img, format("%.2f", probs[i]), Point(boxes[i].x, boxes[i].y - 2),
-                FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0));
-    }
-    imshow("Detection", img);
-    waitKey();
+	for (int i = 0; i < boxes.size(); ++i) {
+		rectangle(img, boxes[i], Scalar(0, 0, 255));
+		rectangle(img, refBoxes[i], Scalar(0, 255, 0));
+		putText(img, format("%.2f", probs[i]), Point(boxes[i].x, boxes[i].y - 2),
+			FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0));
+	}
+	imshow("Detection", img);
+	waitKey();
+
 #endif
 
-    ASSERT_GE(boxes.size(), refBoxes.size());
-    ASSERT_GE(probs.size(), refProbs.size());
-    ASSERT_GE(classes.size(), refBoxes.size());
+	ASSERT_GE(boxes.size(), refBoxes.size());
+	ASSERT_GE(probs.size(), refProbs.size());
+	ASSERT_GE(classes.size(), refBoxes.size());
 
-    int i;
-    for (i = 0; i < boxes.size(); ++i) {
-        if (probs[i] < probThreshold)
-            break;
+	int i;
+	for (i = 0; i < boxes.size(); ++i) {
+		if (probs[i] < probThreshold)
+			break;
 
-        ASSERT_EQ(classes[i], 1);
-        ASSERT_NEAR(probs[i], refProbs[i], 1e-5f);
-        ASSERT_EQ(boxes[i], refBoxes[i]);
-    }
-    ASSERT_EQ(i, boxes.size());
+		ASSERT_EQ(classes[i], 1);
+		ASSERT_EQ(boxes[i], refBoxes[i]);
+		std::cout << "i " << i << std::endl;
+		ASSERT_LE(fabs(probs[i] - refProbs[i]), 1e-5f);
+
+	}
+	ASSERT_EQ(i, boxes.size());
 }
