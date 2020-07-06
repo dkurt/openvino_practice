7906075f0350ff7f27a665cc5ba8a901d4219a51
unknown <dim.trif1234@yandex.ru>
2020-07-06 17:19:02 +0300

    first git-task

--- modules/0_git/test/main.cpp
+++ modules/0_git/test/main.cpp
@@ -6,8 +6,9 @@
 
 TEST(git, say_hello) {
     myspace::A a;
+    A another;
     EXPECT_EQ(myspace::func(a), "Hello, Nizhny!");
-    EXPECT_EQ(func(a), "Hello, World!");
+    EXPECT_EQ(func(another), "Hello, World!");
 }
 
 int main(int argc, char **argv) {
