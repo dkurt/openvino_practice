3682b2c58364b8a93900f5ae2724b94101e68fb4
kirill-kabaev <k.kabaev94@gmail.com>
2020-07-06 20:00:08 +0300

    initial commit

--- modules/0_git/test/main.cpp
+++ modules/0_git/test/main.cpp
@@ -10,6 +10,8 @@ TEST(git, say_hello) {
     EXPECT_EQ(func(a), "Hello, World!");
 }
 
+
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
