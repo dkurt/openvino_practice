e5e4d2d3fb65a317a429ad5be633da52cb266ce3
unknown <dim.trif1234@yandex.ru>
2020-07-07 22:57:08 +0300

    modify countCoins

--- modules/1_opencv/include/opencv_coins.hpp
+++ modules/1_opencv/include/opencv_coins.hpp
@@ -2,5 +2,7 @@
 
 #include <opencv2/opencv.hpp>
 
+double const M_PI = 3.14159265358979323846;
+
 // Count total sum of numbers on the test image with coins
 unsigned countCoins(const cv::Mat& img);
