e4a4f991fd1e1de574ed3d810d68d5f634ea617f
dashachernyh <dasha.chernyh.@mail.ru>
2020-07-08 01:57:38 +0300

    changed

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -1,4 +1,5 @@
 #include "opencv_coins.hpp"
+
 #include<iostream>
 #include<opencv2/opencv.hpp>
 using namespace cv;
@@ -8,34 +9,34 @@ unsigned countCoins(const Mat& img) {
 	double maxVal;
 	cv::Point minLoc;
 	unsigned count_coins=0;
-	cvtColor(img, gray, COLOR_BGR2GRAY);   //принимает исходную картинку img, возвращает тот же img,код цвета серый, канал серый
-	threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);  //Применяет порог фиксированного уровня к каждому элементу массива; gray входной массив,thresh-выходной, пороговое значение от 0 (черный) до 255(белый), тип порога 
+	cvtColor(img, gray, COLOR_BGR2GRAY);   //get source image img, give the same img,channel gray
+	threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);  //aplies a fixed level threshol to each element of the array; gray- input,thresh-ouyput, threashold  0 (black)- 255(white), type threshold 
 
-																	   // Remove unfilled black holes by morphological closing
-	morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);  //Выполняет сложные морфологические преобразования, thresh-исходное изображение (и получаемое), тип морфол операции (сохранение фоновых областей), ядро 3х3, якорная позиция (в центре ядра), 3 - кол-во применений эрозии и расширения 
+	//A complex morphological transformation is performed. Thresh-source image (and received), type of morphol operation (preservation of background areas)
+	morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);  
 
-	//расширение изображения - увеличивает границу обьекта до фона
+	//image extension - increases the border of the object to the background
 	cv::dilate(thresh, thresh, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
 
-	//Вычисляет расстояние до ближайшего нулевого пикселя для каждого пикселя исходного изображения
+	//Calculates the distance to the nearest zero pixel for each pixel in the original image.
 	distanceTransform(thresh, distance_transform, cv::DIST_L2, 5);
 	normalize(distance_transform, distance_transform, 0, 1., NORM_MINMAX);
 
 	/*minMaxLoc(distance_transform, 0, &maxVal, &minLoc);
-	threshold(distance_transform, distance_transform, 7*maxVal,255,0);*/   //все черное
+	threshold(distance_transform, distance_transform, 0.7*maxVal,255,0);*/   //all is black
 	threshold(distance_transform, distance_transform, .45, 1., THRESH_BINARY);
 
-	//создаем версию CV_8U расстояния изображения
+	//create the CV_8U version of the image distance
 	Mat dist_transf_8u;
 	distance_transform.convertTo(dist_transf_8u, CV_8U);
 
-	// создаем вектор, в котром будет хранится контур (изображение котнура)
+	// we create a vector in which the contour will be stored (image of a kotnur)
 	std::vector<std::vector<cv::Point> > contours;
 	cv::findContours(dist_transf_8u, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
-	//считаем кол-во монет
+	//count the number of coins
 	char count = contours.size();
-	putText(distance_transform, std::to_string(count), cv::Point(0, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255));  //вывели кол-во монет на экран
+	putText(distance_transform, std::to_string(count), cv::Point(0, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255));  //brought the number of coins to the picture
 
 	Point2f center[16];
     float radius[16];
@@ -52,16 +53,9 @@ unsigned countCoins(const Mat& img) {
 	  char countC = count_coins;
 	  putText(distance_transform, std::to_string(countC), cv::Point(0, 40), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255));
 	  
-
 	cv::imwrite("newp2.jpg", distance_transform);
-	cv::namedWindow("Img", cv::WINDOW_NORMAL);
-	cv::imshow("Img", distance_transform);
-	cv::waitKey();
-	return count_coins;
-    
-	
-	
 
+	return count_coins;
 	
 }
 
