1019a325f429190b1df259503b374c29e512d980
PlohovEvg <plohov99@yandex.ru>
2020-07-07 22:36:02 +0300

    removed empty rows

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -7,37 +7,24 @@
 using namespace cv;
 
 unsigned countCoins(const Mat& img) {
-	Mat gray, thresh, opening, sure_bg, sure_fg, dist_transform, unknown, markedimg;
+	Mat gray, thresh, sure_bg, sure_fg, dist_transform, unknown, markedimg;
 	Mat kernel = Mat::ones(3, 3, CV_8U);
 	double max = -100000.0;
 	unsigned int result = 0;
     cvtColor(img, gray, COLOR_BGR2GRAY);
     threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
-    // Remove unfilled black holes by morphological closing
-
-    morphologyEx(thresh, thresh, MORPH_CLOSE, kernel, Point(-1, -1), 3);	
-	
-	morphologyEx(thresh, opening, MORPH_OPEN, kernel, Point(-1, -1), 2);	
-	
-	dilate(opening, sure_bg, kernel, Point(-1, -1), 3);
-
-	distanceTransform(thresh, dist_transform, DIST_L2, 5);
-		
+    morphologyEx(thresh, thresh, MORPH_CLOSE, kernel, Point(-1, -1), 3);			
+	dilate(thresh, sure_bg, kernel, Point(-1, -1), 3);
+	distanceTransform(thresh, dist_transform, DIST_L2, 5);		
 	minMaxLoc(dist_transform, NULL, &max);
 	threshold(dist_transform, sure_fg, 0.7*max, 255, 0);	
-
 	sure_fg.convertTo(sure_fg, CV_8U);
 	subtract(sure_bg, sure_fg, unknown);
-
 	std::vector<std::vector<cv::Point> > ar;
-
 	findContours(sure_fg, ar, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-
 	double Radius;
-
 	//1 Rouble coin radius:10,25 mm
 	//2 Rouble coin radius:11,5 mm
-
 	for (int i = 0; i < ar.size(); i++)
 	{
 		Radius = sqrt(contourArea(ar[i]) / M_PI);
