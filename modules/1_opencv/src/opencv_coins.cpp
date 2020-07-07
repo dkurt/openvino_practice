78b45362e7cdf4e88eb832d20d8850e482825106
Zheleztsova Irina <irina.z98@mail.ru>
2020-07-07 19:27:50 +0300

    initial commit

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -1,4 +1,6 @@
 #include "opencv_coins.hpp"
+#include <opencv2/imgproc.hpp>
+#include <cmath>
 
 using namespace cv;
 
@@ -10,22 +12,22 @@ unsigned countCoins(const Mat& img) {
     morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
 
     // TODO: implement an algorithm from https://docs.opencv.org/master/d3/db4/tutorial_py_watershed.html
-    CV_Error(Error::StsNotImplemented, "countCoins");
     dilate(thresh, thresh, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
     distanceTransform(thresh, thresh, DIST_L2, 5);
-    double dist_transform;
-    minMaxLoc(thresh, &dist_transform);
-    threshold(thresh, thresh, 0.7 * dist_transform, 255.0, THRESH_BINARY);
-    thresh.convertTo(thresh, CV_8U);
+    double dist_transform, minVal;
+    Point min_Loc, max_Loc;
+    minMaxLoc(thresh, &minVal, &dist_transform, &min_Loc, &max_Loc);
+    threshold(thresh, thresh, dist_transform*0.7, 255, THRESH_BINARY);
+    thresh.convertTo(thresh, CV_8U, 1, 0);
     std::vector<std::vector<cv::Point> > contours;
     cv::findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
     int ncomp = contours.size();
     int sum = 0;
     double radius;
     for (int i = 0; i < ncomp; ++i) {
-        radius = sqrt(contourArea(contours[i]) /3.14);
+        radius = sqrt(contourArea(contours[i]) / 3.14);
         if (radius > 11) {
-            sum += 2;
+            sum + +2;
         }
         else {
             sum += 1;
@@ -33,4 +35,3 @@ unsigned countCoins(const Mat& img) {
     }
     return sum;
 }
-
