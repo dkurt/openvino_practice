0a54d2d7ba995288db07a7a840b9c01b6a706b19
kirill-kabaev <k.kabaev94@gmail.com>
2020-07-07 22:39:27 +0300

    initial commit

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -19,7 +19,6 @@ unsigned countCoins(const Mat& img) {
     double maxVal;
     minMaxLoc(dist_transform, 0, &maxVal);
     threshold(dist_transform, sure_fg, 0.7 * maxVal, 255, 0);
-    Mat errorImage;
     sure_fg.convertTo(sure_fg, CV_8U);
     std::vector<std::vector<Point>> contours;
     findContours(sure_fg, contours, RETR_LIST, CHAIN_APPROX_NONE);
@@ -27,7 +26,7 @@ unsigned countCoins(const Mat& img) {
     for (int i = 0; i < contours.size(); i++)
     {
         double r = sqrt(contourArea(contours[i]) / CV_PI);
-        if (r > 11)
+        if (r > 13)
             sum += 2;
         else
             sum++;
