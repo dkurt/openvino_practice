75d7850ed9cf7288ba812e2247e50a6c88cd22cc
ignatovskiy <naignatovskiy@edu.hse.ru>
2020-07-07 13:27:21 +0300

    OpenCV practice 2nd attempt

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -3,10 +3,10 @@
 using namespace cv;
 
 unsigned countCoins(const Mat& img) {
-    Mat gray, thresh, bkground, unknown;
+    Mat gray, thresh, bkground;
     cvtColor(img, gray, COLOR_BGR2GRAY);
     threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
-    // Remove unfilled black holes by morphological closing
+
     morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
 
     dilate(thresh, bkground, 3);
@@ -39,8 +39,4 @@ unsigned countCoins(const Mat& img) {
     }
 
     return coinsValue;
-
-
-    // TODO: implement an algorithm from https://docs.opencv.org/master/d3/db4/tutorial_py_watershed.html
-    CV_Error(Error::StsNotImplemented, "countCoins");
 }
