61fbd115d90869872547d6a08cca9dfc5b4934e6
ilyaShein <ilya.shein.1999@mail.ru>
2020-07-07 22:15:50 +0300

    new 1_opencv

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -3,24 +3,24 @@
 using namespace cv;
 
 unsigned countCoins(const Mat& img) {
-	   Mat gray, thresh;
-   	cvtColor(img, gray, COLOR_BGR2GRAY);
-	   threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
-	   morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
-	   dilate(thresh, thresh, 3);
-	   distanceTransform(thresh, thresh, DIST_L2, 5);
+    Mat gray, thresh;
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+    threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
+    morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
+    dilate(thresh, thresh, 3);
+    distanceTransform(thresh, thresh, DIST_L2, 5);
 
-	   double maxValue, minValue;
-	   Point maxLoc, minLoc;
-	   minMaxLoc(thresh, &minValue, &maxValue, &minLoc, &maxLoc);
-	   threshold(thresh, thresh, 0.7*maxValue, 255, 0);
+    double maxValue, minValue;
+    Point maxLoc, minLoc;
+    minMaxLoc(thresh, &minValue, &maxValue, &minLoc, &maxLoc);
+    threshold(thresh, thresh, 0.7*maxValue, 255, 0);
 
-	   thresh.convertTo(thresh, CV_8U);
-	   std::vector<std::vector<Point> > contours;
-	   findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    thresh.convertTo(thresh, CV_8U);
+    std::vector<std::vector<Point> > contours;
+    findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
-	   int Sum = 0;
-	   for (int i = 0; i < contours.size(); i++){
+    int Sum = 0;
+    for (int i = 0; i < contours.size(); i++){
        double radius = arcLength(contours[i], true) / (2 * CV_PI);
        if (radius > 15){
           Sum += 2;
