e720d942038f7264e7e4900e548fdef58f2c6574
WolfCub15 <volkovanastyavolkova@gmail.com>
2020-07-08 00:13:03 +0300

    sum of money

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -1,4 +1,5 @@
 #include "opencv_coins.hpp"
+#include <cmath>
 
 using namespace cv;
 
@@ -9,6 +10,25 @@ unsigned countCoins(const Mat& img) {
     // Remove unfilled black holes by morphological closing
     morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
 
-    // TODO: implement an algorithm from https://docs.opencv.org/master/d3/db4/tutorial_py_watershed.html
-    CV_Error(Error::StsNotImplemented, "countCoins");
+    dilate(thresh, thresh, 3);
+    distanceTransform(thresh, thresh, DIST_L2, 5);
+    double min_value, max_value;
+    Point minLoc, maxLoc;
+    minMaxLoc(thresh, &min_value, &max_value, &minLoc, &maxLoc);
+    threshold(thresh, thresh, max_value * 0.7, 255, THRESH_BINARY);
+
+    thresh.convertTo(thresh, CV_8U);
+
+    std::vector<std::vector<Point>> money_contours;
+   // imshow("image", thresh);
+    //waitKey();
+    findContours(thresh, money_contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    int sum = 0;
+    for (int i = 0; i < money_contours.size(); i++) {
+        double radius = sqrt(contourArea(money_contours[i]) / CV_PI);
+        if (radius > 11) sum += 2;
+        else sum ++;
+    }
+    return sum;
 }
