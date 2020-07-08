e7c2537330c709ed3a9952c3f00de68081ff6c63
dolgopolov-d <danildol2011@yandex.ru>
2020-07-08 18:51:13 +0300

    to validate

--- modules/2_mnist/src/mnist.cpp
+++ modules/2_mnist/src/mnist.cpp
@@ -21,6 +21,22 @@ void loadImages(const std::string& filepath,
     CV_CheckEQ(magicNum, 2051, "");
 
     int numImages = readInt(ifs);
+	int numRows = readInt(ifs);
+	int numColumns = readInt(ifs);
+	for (int i = 0; i < numImages; i++)
+	{
+		Mat tmp(numRows, numColumns, CV_8UC1);
+		for (int j = 0; j < numRows; j++)
+		{
+			for (int f = 0; f < numColumns; f++)
+			{
+				unsigned char val = 0;
+				ifs.read((char*)&val, 1);
+				tmp.at<unsigned char>(j, f) = val;
+			}
+		}
+		images.push_back(tmp);
+	}
 
     // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
     // at http://yann.lecun.com/exdb/mnist/
@@ -35,18 +51,36 @@ void loadLabels(const std::string& filepath,
     CV_CheckEQ(magicNum, 2049, "");
 
     int numLabels = readInt(ifs);
+	for (int i = 0; i < numLabels; i++)
+	{
+		unsigned char val = 0;
+		ifs.read((char*)&val, 1);
+		labels.push_back(val);
+	}
 
     // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
     // at http://yann.lecun.com/exdb/mnist/
 }
 
 void prepareSamples(const std::vector<cv::Mat>& images, cv::Mat& samples) {
-    CV_Error(Error::StsNotImplemented, "prepareSamples");
+	int i = images.size();
+	Mat sample(i, 28 * 28, CV_32FC1);
+	for (int j = 0; j < i; j++)
+	{
+		Mat image_row = images[j].clone().reshape(1, 1);
+		Mat row_j = sample.row(j);
+		image_row.convertTo(row_j, CV_32FC1);
+	}
+	samples = sample;
 }
 
 Ptr<ml::KNearest> train(const std::vector<cv::Mat>& images,
                         const std::vector<int>& labels) {
-    CV_Error(Error::StsNotImplemented, "train");
+	Ptr <ml::KNearest> a = ml::KNearest::create();
+	Mat sample;
+	prepareSamples(images, sample);
+	a->train(sample, ml::ROW_SAMPLE, labels);
+	return a;
 }
 
 float validate(Ptr<ml::KNearest> model,
