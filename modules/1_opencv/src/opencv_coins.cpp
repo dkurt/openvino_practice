3a25af552ceb2283d2a03db08363bc764d6ad0e8
Gourdji19 <knappa@mail.ru>
2020-07-08 12:58:57 +0300

    fixed strings and last newline

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -2,14 +2,11 @@
 using namespace cv;
 unsigned countCoins(const Mat& img) {
 	cv::Mat image = cv::imread("C:\\Users\\Alexandra\\openvino_practice\\data\\coins.jpg");
-	Mat gray, thresh;
+	Mat gray, thresh,sure_bg;
 	int sum = 0;
 	cvtColor(img, gray, COLOR_BGR2GRAY);
 	threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
-	// Remove unfilled black holes by morphological closing
 	morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
-	// TODO: implement an algorithm from https://docs.opencv.org/master/d3/db4/tutorial_py_watershed.html
-	CV_Error(Error::StsNotImplemented, "countCoins");
 	dilate(thresh, thresh, 3);
 	distanceTransform(thresh, thresh, DIST_L2, 5);
 	double maxValue, minValue;
@@ -21,7 +18,7 @@ unsigned countCoins(const Mat& img) {
 	findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 	for (int i = 0; i < contours.size(); i++)
 	{
-		double radius = sqrt(contourArea(contours[i]) / CV_PI);
+		double radius = std::sqrt(contourArea(contours[i]) / CV_PI);
 		if (radius > 12)
 		{
 			sum += 2;
