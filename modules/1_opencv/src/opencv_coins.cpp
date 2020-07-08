31f59cbc936b06d218a5d0e64281412257f46703
Zheleztsova Irina <irina.z98@mail.ru>
2020-07-08 12:50:10 +0300

    initial commit

--- modules/1_opencv/src/opencv_coins.cpp
+++ modules/1_opencv/src/opencv_coins.cpp
@@ -8,26 +8,21 @@ unsigned countCoins(const Mat& img) {
     Mat gray, thresh;
     cvtColor(img, gray, COLOR_BGR2GRAY);
     threshold(gray, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
-    // Remove unfilled black holes by morphological closing
     morphologyEx(thresh, thresh, MORPH_CLOSE, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
-
-    // TODO: implement an algorithm from https://docs.opencv.org/master/d3/db4/tutorial_py_watershed.html
-    dilate(thresh, thresh, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
     distanceTransform(thresh, thresh, DIST_L2, 5);
-    double dist_transform, minVal;
-    Point min_Loc, max_Loc;
-    minMaxLoc(thresh, &minVal, &dist_transform, &min_Loc, &max_Loc);
-    threshold(thresh, thresh, dist_transform*0.7, 255, THRESH_BINARY);
-    thresh.convertTo(thresh, CV_8U, 1, 0);
+    double dist_transform = 0.0;
+    minMaxLoc(thresh, 0, &dist_transform);
+    threshold(thresh, thresh, 0.7 * dist_transform, 255, THRESH_BINARY);
+    thresh.convertTo(thresh, CV_8U);
     std::vector<std::vector<cv::Point> > contours;
     cv::findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
     int ncomp = contours.size();
     int sum = 0;
     double radius;
     for (int i = 0; i < ncomp; ++i) {
-        radius = sqrt(contourArea(contours[i]) / 3.14);
+        radius = sqrt(contourArea(contours[i]) / CV_PI);
         if (radius > 11) {
-            sum + +2;
+            sum += 2;
         }
         else {
             sum += 1;
