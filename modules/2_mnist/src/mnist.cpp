35eb38e9b42a23d03203575851609338a71e9a30
Shvec_D <schwets.mitia-shvets@yandex.ru>
2020-07-09 12:25:51 +0300

    had done loadLabels

--- modules/2_mnist/src/mnist.cpp
+++ modules/2_mnist/src/mnist.cpp
@@ -4,83 +4,87 @@
 using namespace cv;
 
 inline int readInt(std::ifstream& ifs) {
-    int val;
-    ifs.read((char*)&val, 4);
-    // Integers in file are high endian which requires swap
-    std::swap(((char*)&val)[0], ((char*)&val)[3]);
-    std::swap(((char*)&val)[1], ((char*)&val)[2]);
-    return val;
+	int val;
+	ifs.read((char*)&val, 4);
+	// Integers in file are high endian which requires swap
+	std::swap(((char*)&val)[0], ((char*)&val)[3]);
+	std::swap(((char*)&val)[1], ((char*)&val)[2]);
+	return val;
 }
 
 void loadImages(const std::string& filepath,
-                std::vector<Mat>& images) {
-    std::ifstream ifs(filepath.c_str(), std::ios::binary);
-    CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
+	std::vector<Mat>& images) {
+	std::ifstream ifs(filepath.c_str(), std::ios::binary);
+	CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
 
-    int magicNum = readInt(ifs);
-    CV_CheckEQ(magicNum, 2051, "");
+	int magicNum = readInt(ifs);
+	CV_CheckEQ(magicNum, 2051, "");
 
-    int numImages = readInt(ifs);
-    int numRows = readInt(ifs);
-    int numColumns = readInt(ifs);
+	int numImages = readInt(ifs);
+	int numRows = readInt(ifs);
+	int numColumns = readInt(ifs);
 
-    for (int i = 0; i < numImages; i++)
-    {
-        Mat t(numRows, numColumns, CV_8UC1);
-        for (int j = 0; j < numRows; j++)
-        {
-            for (int k = 0; k < numColumns; k++)
-            {
-                char tmp;
-                ifs.read((char*)&tmp, 1);
-                t.at<char>(j, k) = tmp;
+	for (int i = 0; i < numImages; i++)
+	{
+		Mat t(numRows, numColumns, CV_8UC1);
+		for (int j = 0; j < numRows; j++)
+		{
+			for (int k = 0; k < numColumns; k++)
+			{
+				char tmp;
+				ifs.read((char*)&tmp, 1);
+				t.at<char>(j, k) = tmp;
 
-            }
+			}
 
-        }
-        images.push_back(t);
-    }
+		}
+		images.push_back(t);
+	}
 }
 
 void loadLabels(const std::string& filepath,
-                std::vector<int>& labels) {
-    std::ifstream ifs(filepath.c_str(), std::ios::binary);
-    CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
+	std::vector<int>& labels) {
+	std::ifstream ifs(filepath.c_str(), std::ios::binary);
+	CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
 
-    int magicNum = readInt(ifs);
-    CV_CheckEQ(magicNum, 2049, "");
+	int magicNum = readInt(ifs);
+	CV_CheckEQ(magicNum, 2049, "");
 
-    int numLabels = readInt(ifs);
+	int numLabels = readInt(ifs);
+	for (int k = 0; k < numLabels; k++)
+	{
+		char tmp;
+		ifs.read((char*)&tmp, 1);
+		labels.push_back(tmp);
 
-    // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
-    // at http://yann.lecun.com/exdb/mnist/
+	}
 }
 
 void prepareSamples(const std::vector<cv::Mat>& images, cv::Mat& samples) {
-    CV_Error(Error::StsNotImplemented, "prepareSamples");
+	CV_Error(Error::StsNotImplemented, "prepareSamples");
 }
 
 Ptr<ml::KNearest> train(const std::vector<cv::Mat>& images,
-                        const std::vector<int>& labels) {
-    CV_Error(Error::StsNotImplemented, "train");
+	const std::vector<int>& labels) {
+	CV_Error(Error::StsNotImplemented, "train");
 }
 
 float validate(Ptr<ml::KNearest> model,
-               const std::vector<cv::Mat>& images,
-               const std::vector<int>& labels) {
-    CV_Error(Error::StsNotImplemented, "validate");
+	const std::vector<cv::Mat>& images,
+	const std::vector<int>& labels) {
+	CV_Error(Error::StsNotImplemented, "validate");
 }
 
 int predict(Ptr<ml::KNearest> model, const Mat& image) {
-    // TODO: resize image to 28x28 (cv::resize)
+	// TODO: resize image to 28x28 (cv::resize)
 
-    // TODO: convert image from BGR to HSV (cv::cvtColor)
+	// TODO: convert image from BGR to HSV (cv::cvtColor)
 
-    // TODO: get Saturate component (cv::split)
+	// TODO: get Saturate component (cv::split)
 
-    // TODO: prepare input - single row FP32 Mat
+	// TODO: prepare input - single row FP32 Mat
 
-    // TODO: make a prediction by the model
+	// TODO: make a prediction by the model
 
-    CV_Error(Error::StsNotImplemented, "predict");
+	CV_Error(Error::StsNotImplemented, "predict");
 }
