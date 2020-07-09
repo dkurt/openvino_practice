9721d9ae09ff65fbec17f3514c88cd7741a53eec
ShuryginaA <annashurygina353@gmail.com>
2020-07-09 16:23:37 +0300

    fixed loading

--- modules/2_mnist/src/mnist.cpp
+++ modules/2_mnist/src/mnist.cpp
@@ -6,7 +6,6 @@ using namespace cv;
 inline int readInt(std::ifstream& ifs) {
     int val;
     ifs.read((char*)&val, 4);
-    // Integers in file are high endian which requires swap
     std::swap(((char*)&val)[0], ((char*)&val)[3]);
     std::swap(((char*)&val)[1], ((char*)&val)[2]);
     return val;
@@ -15,61 +14,49 @@ inline int readInt(std::ifstream& ifs) {
 void loadImages(const std::string& filepath,
                 std::vector<Mat>& images) {
     std::ifstream ifs(filepath.c_str(), std::ios::binary);
-    /*CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
+    CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
 
     int magicNum = readInt(ifs);
-    CV_CheckEQ(magicNum, 2051, "");*/
+    CV_CheckEQ(magicNum, 2051, "");
 
     int numImages = readInt(ifs);
-
-    // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
-    // at http://yann.lecun.com/exdb/mnist/
-
-    // reading img and convert mat labels to int
     int rows = readInt(ifs);
     int cols = readInt(ifs);
-    char val = 0;
-
-    //std::cout << rows<<cols << std::endl;
 
     for (int i = 0; i < numImages; i++) {
         Mat img(rows, cols, CV_8UC1);
         for (int j = 0; j < rows; j++) {
             for (int k = 0; k < cols; k++) {
+                char val = 0;
                 ifs.read(&val, sizeof(val));
                 img.at<char>(j, k) = val;
             }
         }
         images.push_back(img);
-        //img.release();
+        img.release();
     }
 
 }
 
 void loadLabels(const std::string& filepath,
-                std::vector<int>& labels) {
+    std::vector<int>& labels) {
     std::ifstream ifs(filepath.c_str(), std::ios::binary);
-    /*CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
+    CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
 
     int magicNum = readInt(ifs);
-    CV_CheckEQ(magicNum, 2049, "");*/
-
+    CV_CheckEQ(magicNum, 2049, "");
+    
     int numLabels = readInt(ifs);
-
-    // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
-    // at http://yann.lecun.com/exdb/mnist/
-
+    labels = std::vector<int>(numLabels);
     for (int i = 0; i < numLabels; i++) {
         char val = 0;
-        ifs.read(&val, sizeof(val));
-        labels.push_back(val);
+        ifs.read((char*)&val, 1);
+        labels[i] = val;
     }
+
 }
 
 void prepareSamples(const std::vector<cv::Mat>& images, cv::Mat& samples) {
-    
-   /* CV_Error(Error::StsNotImplemented, "prepareSamples");  */ //!!!!!!!!!!!
-
     samples = Mat::zeros(images.size(), images[0].rows * images[0].cols, CV_32FC1);
     for (int i = 0; i < images.size(); i++) {
         for (int j = 0; j < images[i].rows; j++) {
@@ -81,21 +68,17 @@ void prepareSamples(const std::vector<cv::Mat>& images, cv::Mat& samples) {
 }
 
 Ptr<ml::KNearest> train(const std::vector<cv::Mat>& images,
-                        const std::vector<int>& labels) {
-   /* CV_Error(Error::StsNotImplemented, "train");         */  //!!!!!!!!!!!!
+    const std::vector<int>& labels) {
     Ptr <ml::KNearest> model = cv::ml::KNearest::create();
     Mat samples;
     prepareSamples(images, samples);
     model->train(samples, ml::ROW_SAMPLE, labels);
     return model;
-
-
 }
 
 float validate(Ptr<ml::KNearest> model,
                const std::vector<cv::Mat>& images,
                const std::vector<int>& labels) {
-    /*CV_Error(Error::StsNotImplemented, "validate");*/
 
     Mat samples;
     prepareSamples(images, samples);
@@ -111,28 +94,13 @@ float validate(Ptr<ml::KNearest> model,
 }
 
 int predict(Ptr<ml::KNearest> model, const Mat& image) {
-    // TODO: resize image to 28x28 (cv::resize)
-
     Mat pred;
     resize(image, pred, Size(28, 28));
-
-    // TODO: convert image from BGR to HSV (cv::cvtColor)
-
     cvtColor(pred, pred, COLOR_BGR2HSV);
-
-    // TODO: get Saturate component (cv::split)
-
-    // TODO: prepare input - single row FP32 Mat
-
     Mat channels[3];
     split(pred, &channels[0]);
     std::vector<Mat> sat_comp(1, channels[1]);
-
-    // TODO: make a prediction by the model
     prepareSamples(sat_comp, pred);
-
-    //CV_Error(Error::StsNotImplemented, "predict");
-
     int res = model->predict(pred);
     return res;
 }
