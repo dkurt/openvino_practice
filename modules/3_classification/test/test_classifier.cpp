6bdbb8ac3b31b2a46b6adf43ec71b2bbeb03442b
ignatovskiy <naignatovskiy@edu.hse.ru>
2020-07-10 13:55:12 +0300

    Revert "iou and nms (2/3 tests)"
    This reverts commit e984dcce372a012300aac865055b113d11986a74.

--- modules/3_classification/test/test_classifier.cpp
+++ modules/3_classification/test/test_classifier.cpp
@@ -67,7 +67,7 @@ TEST(classification, SoftMaxLarge) {
 // In this test run image classification network and get top 5 classes with the
 // highest probabilities. Use implemented topK and SoftMax methods to pass tests
 TEST(classification, DenseNet) {
-    Mat image = imread(join(DATA_FOLDER, "carusel3.jpg"));
+    Mat image = imread(join(DATA_FOLDER, "tram.jpg"));
     std::vector<float> top5_scores;
     std::vector<unsigned> top5_classes;
 
--- modules/4_detection/src/detector.cpp
+++ modules/4_detection/src/detector.cpp
@@ -15,6 +15,7 @@ Detector::Detector() {
                               utils::fs::join(DATA_FOLDER, "face-detection-0104.bin"));
 }
 
+
 void Detector::detect(const cv::Mat& image,
                       float nmsThreshold,
                       float probThreshold,
@@ -27,45 +28,9 @@ void Detector::detect(const cv::Mat& image,
 
 void nms(const std::vector<cv::Rect>& boxes, const std::vector<float>& probabilities,
          float threshold, std::vector<unsigned>& indices) {
-    std::vector<Rect> boxesCopy = boxes;
-    std::vector<float> probsCopy = probabilities;
-    std::vector<Rect> approvedBoxes;
-
-    while (boxesCopy.size() > 0)
-    {
-        float tempprob = *std::max_element(probsCopy.begin(), probsCopy.end());
-        auto iteratorFinding = std::find(probsCopy.begin(), probsCopy.end(), tempprob);
-        int indexFinding = iteratorFinding - probsCopy.begin();
-
-        Rect currentRect = boxesCopy[indexFinding];
-
-        boxesCopy.erase(boxesCopy.begin() + indexFinding);
-        probsCopy.erase(probsCopy.begin() + indexFinding);
-
-        approvedBoxes.push_back(currentRect);
-
-        for (int i = 0; i < boxesCopy.size(); i++)
-        {
-            float iou_metric = iou(currentRect, boxesCopy[i]);
-            if (iou_metric > threshold)
-            {
-                boxesCopy.erase(boxesCopy.begin() + i);
-                probsCopy.erase(probsCopy.begin() + i);
-            }
-        }
-    }
-
-    for (int i = 0; i < approvedBoxes.size(); i++)
-    {
-        Rect tempRect = approvedBoxes[i];
-        auto iteratorFinding = std::find(boxes.begin(), boxes.end(), tempRect);
-        int indexFinding = std::distance(boxes.begin(), iteratorFinding);
-        indices.push_back(indexFinding);
-    }
+    CV_Error(Error::StsNotImplemented, "nms");
 }
 
 float iou(const cv::Rect& a, const cv::Rect& b) {
-    float intersectionRects = (a & b).area();
-    float unionRects = a.area() + b.area();
-    return intersectionRects / (unionRects - intersectionRects);
+    CV_Error(Error::StsNotImplemented, "iou");
 }
