ec4b5f2ed857444cbb954445aea99531c86c983a
unknown <dim.trif1234@yandex.ru>
2020-07-10 19:57:27 +0300

    initial commit

--- modules/3_classification/src/classifier.cpp
+++ modules/3_classification/src/classifier.cpp
@@ -9,20 +9,43 @@ using namespace cv;
 using namespace cv::utils::fs;
 
 void topK(const std::vector<float>& src, unsigned k,
-          std::vector<float>& dst,
-          std::vector<unsigned>& indices) {
-    CV_Error(Error::StsNotImplemented, "topK");
+    std::vector<float>& dst,
+    std::vector<unsigned>& indices) {
+
+    std::map<float, int> valueIdx;
+    for (int i = 0; i < src.size(); ++i) {
+        valueIdx.insert(std::make_pair(src[i], i));
+    }
+
+    std::map<float, int>::iterator iter = valueIdx.end();
+    for (int i = 0; i < k; ++i) {
+        --iter;
+        indices.push_back(iter->second);
+        dst.push_back(iter->first);
+    }
 }
 
 void softmax(std::vector<float>& values) {
-    CV_Error(Error::StsNotImplemented, "softmax");
+
+    float denominator = 0.0f;
+
+    auto maxValue = std::max_element(values.begin(), values.end());
+
+    for (int i = 0; i < values.size(); ++i) {
+        values[i] -= *maxValue;
+        denominator += exp(values[i]);
+    }
+
+    for (int i = 0; i < values.size(); ++i) {
+        values[i] = cv::exp(values[i]) / denominator;
+    }
 }
 
 Blob::Ptr wrapMatToBlob(const Mat& m) {
     CV_Assert(m.depth() == CV_8U);
-    std::vector<size_t> dims = {1, (size_t)m.channels(), (size_t)m.rows, (size_t)m.cols};
+    std::vector<size_t> dims = { 1, (size_t)m.channels(), (size_t)m.rows, (size_t)m.cols };
     return make_shared_blob<uint8_t>(TensorDesc(Precision::U8, dims, Layout::NHWC),
-                                     m.data);
+        m.data);
 }
 
 Classifier::Classifier() {
@@ -30,7 +53,7 @@ Classifier::Classifier() {
 
     // Load deep learning network into memory
     CNNNetwork net = ie.ReadNetwork(join(DATA_FOLDER, "DenseNet_121.xml"),
-                                    join(DATA_FOLDER, "DenseNet_121.bin"));
+        join(DATA_FOLDER, "DenseNet_121.bin"));
 
     // Specify preprocessing procedures
     // (NOTE: this part is different for different models!)
@@ -48,7 +71,7 @@ Classifier::Classifier() {
 }
 
 void Classifier::classify(const cv::Mat& image, int k, std::vector<float>& probabilities,
-                          std::vector<unsigned>& indices) {
+    std::vector<unsigned>& indices) {
     // Create 4D blob from BGR image
     Blob::Ptr input = wrapMatToBlob(image);
 
@@ -60,4 +83,11 @@ void Classifier::classify(const cv::Mat& image, int k, std::vector<float>& proba
 
     // Copy output. "prob" is a name of output from .xml file
     float* output = req.GetBlob(outputName)->buffer();
+    int size = req.GetBlob(outputName)->size();
+    std::vector<float> src(size);
+    for (int i = 0; i < size; ++i) {
+        src[i] = output[i];
+    }
+    topK(src, k, probabilities, indices);
+    softmax(probabilities);
 }
