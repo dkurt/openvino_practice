546aad7a7e293ae620b9a330ab304b16f8457731
Vladislav-Alex <vladislav.tischenko@mail.ru>
2020-07-10 18:16:41 +0300

    secondTask

--- modules/2_mnist/src/mnist.cpp
+++ modules/2_mnist/src/mnist.cpp
@@ -13,7 +13,7 @@ inline int readInt(std::ifstream& ifs) {
 }
 
 void loadImages(const std::string& filepath,
-                std::vector<Mat>& images) {
+    std::vector<Mat>& images) {
     std::ifstream ifs(filepath.c_str(), std::ios::binary);
     CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
 
@@ -21,13 +21,24 @@ void loadImages(const std::string& filepath,
     CV_CheckEQ(magicNum, 2051, "");
 
     int numImages = readInt(ifs);
+    int x = readInt(ifs);
+    int y = readInt(ifs);
 
-    // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
-    // at http://yann.lecun.com/exdb/mnist/
+    for (int i = 0; i < numImages; ++i) {
+        Mat image(x, y, CV_8UC1);
+        for (int j = 0; j < x; ++j) {
+            for (int k = 0; k < y; ++k) {
+                uchar value;
+                ifs.read((char*)&value, sizeof(value));
+                image.at<uchar>(j, k) = value;
+            }
+        }
+        images.push_back(image);
+    }
 }
 
 void loadLabels(const std::string& filepath,
-                std::vector<int>& labels) {
+    std::vector<int>& labels) {
     std::ifstream ifs(filepath.c_str(), std::ios::binary);
     CV_CheckEQ(ifs.is_open(), true, filepath.c_str());
 
@@ -36,35 +47,58 @@ void loadLabels(const std::string& filepath,
 
     int numLabels = readInt(ifs);
 
-    // TODO: follow "FILE FORMATS FOR THE MNIST DATABASE" specification
-    // at http://yann.lecun.com/exdb/mnist/
+    for (int i = 0; i < numLabels; ++i) {
+        uchar value;
+        ifs.read((char*)&value, sizeof(value));
+        labels.push_back(value);
+    }
 }
 
 void prepareSamples(const std::vector<cv::Mat>& images, cv::Mat& samples) {
-    CV_Error(Error::StsNotImplemented, "prepareSamples");
+    samples = Mat::zeros(images.size(), images[0].rows * images[0].cols, CV_32FC1);
+    for (int i = 0; i < images.size(); ++i) {
+        for (int j = 0; j < images[0].rows; ++j) {
+            for (int k = 0; k < images[0].cols; ++k) {
+                samples.at<double>(i, j * images[0].cols + k) = (double)images[i].at<uint8_t>(j, k);
+            }
+        }
+    }
+    //CV_Error(Error::StsNotImplemented, "prepareSamples");
 }
 
-Ptr<ml::KNearest> train(const std::vector<cv::Mat>& images,
-                        const std::vector<int>& labels) {
+Ptr<ml::KNearest> train(const std::vector<cv::Mat>& images, const std::vector<int>& labels) {
+    auto newModel = cv::ml::KNearest::create();
+    Mat temp;
+    prepareSamples(images, temp);
+    newModel->train(temp, ml::ROW_SAMPLE, labels);
+    return newModel;
     CV_Error(Error::StsNotImplemented, "train");
 }
 
-float validate(Ptr<ml::KNearest> model,
-               const std::vector<cv::Mat>& images,
-               const std::vector<int>& labels) {
+float validate(Ptr<ml::KNearest> model, const std::vector<cv::Mat>& images, const std::vector<int>& labels) {
+    Mat temp;
+    prepareSamples(images, temp);
+    Mat test;
+    model->predict(temp, test);
+    std::vector<int> copy(test.clone());
+    int total = 0;
+    for (int k = 0; k < labels.size(); ++k) {
+        if (labels[k] == copy[k]) total++;
+    }
+    float var = (float)total / (float)labels.size();
+    return var;
     CV_Error(Error::StsNotImplemented, "validate");
 }
 
 int predict(Ptr<ml::KNearest> model, const Mat& image) {
-    // TODO: resize image to 28x28 (cv::resize)
-
-    // TODO: convert image from BGR to HSV (cv::cvtColor)
-
-    // TODO: get Saturate component (cv::split)
-
-    // TODO: prepare input - single row FP32 Mat
-
-    // TODO: make a prediction by the model
-
+    Mat temp;
+    cv::resize(image, temp, Size(28, 28));
+    cv::cvtColor(temp, temp, COLOR_BGR2HSV);
+    Mat chanel[3];
+    cv::split(temp, &chanel[0]);
+    std::vector<Mat> img;
+    img.push_back(chanel[1]);
+    prepareSamples(img, temp);
+    return (int)model->predict(temp);
     CV_Error(Error::StsNotImplemented, "predict");
 }
