3bdf1ae417daa39b58a55f1c761e2add3ec8678d
unknown <dim.trif1234@yandex.ru>
2020-07-12 15:07:40 +0300

    added iou

--- modules/4_detection/src/detector.cpp
+++ modules/4_detection/src/detector.cpp
@@ -28,9 +28,25 @@ void Detector::detect(const cv::Mat& image,
 
 void nms(const std::vector<cv::Rect>& boxes, const std::vector<float>& probabilities,
          float threshold, std::vector<unsigned>& indices) {
+    std::map<float, int> probIdx;
+    for (int i = 0; i < probabilities.size(); ++i){
+        if (probabilities[i] >= threshold) {
+            probIdx.insert(std::make_pair(probabilities[i], i));
+        }
+    }
+    indices.resize(probIdx.size());
+    auto iter = probIdx.end();
+    for (size_t i = 0; i < probIdx.size(); ++i){
+        --iter;
+        indices[i] = iter->second;
+    }
+
     CV_Error(Error::StsNotImplemented, "nms");
 }
 
 float iou(const cv::Rect& a, const cv::Rect& b) {
-    CV_Error(Error::StsNotImplemented, "iou");
+
+    float inner_area = (a & b).area();
+    float onner_area = a.area() + b.area();
+    return inner_area / (onner_area - inner_area);
 }
