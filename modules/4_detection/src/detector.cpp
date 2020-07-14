f8ab88eacf54e5a96e356c635397337ee15705be
dashachernyh <dasha.chernyh.@mail.ru>
2020-07-14 15:17:07 +0300

    detect

--- modules/4_detection/src/detector.cpp
+++ modules/4_detection/src/detector.cpp
@@ -37,13 +37,13 @@ Detector::Detector() {
 
 
 void Detector::detect(const cv::Mat& image,
-                      float nmsThreshold,
-                      float probThreshold,
-                      std::vector<cv::Rect>& boxes,
-                      std::vector<float>& probabilities,
-                      std::vector<unsigned>& classes) {
+	float nmsThreshold,
+	float probThreshold,
+	std::vector<cv::Rect>& boxes,
+	std::vector<float>& probabilities,
+	std::vector<unsigned>& classes){
 	// Create 4D blob from BGR image
-	
+
 	Blob::Ptr input = wrapMatToBlob(image);
 
 	// Pass blob as network's input. "data" is a name of input from .xml file
@@ -55,72 +55,84 @@ void Detector::detect(const cv::Mat& image,
 	// Copy output. "prob" is a name of output from .xml file
 	float* output = req.GetBlob(outputName)->buffer();
 
-	int N= req.GetBlob(outputName)->size()/7;
+	int N = req.GetBlob(outputName)->size() / 7;
+
 	std::vector<cv::Rect> tmp_boxes;
 	std::vector<float> tmp_probabilities;
 	std::vector<unsigned> tmp_classes;
+
 	for (int i = 0; i < N; i++)
 	{
-		if (output[i + 2] >= probThreshold);
+		if (output[i * 7 + 2] >= probThreshold)
 		{
-			int x1 = output[i*7 + 3] * image.cols;
-			int y1 = output[i*7 + 4] * image.rows;
-			int x2 = output[i*7 + 5] * image.cols;
-			int y2 = output[i*7 + 6] * image.rows;
-		
-			cv::Rect p(x1, y1, x2-x1 + 1, y2-y1 + 1);
+		  cv::Rect p(static_cast<int>(output[i * 7 + 3] * image.cols), static_cast<int>(output[i * 7 + 4] * image.rows), static_cast<int>(output[i * 7 + 5] * image.cols) - static_cast<int>(output[i * 7 + 3] * image.cols) + 1,static_cast<int>(output[i * 7 + 6] * image.rows) - static_cast<int>(output[i * 7 + 4] * image.rows) + 1);
+	
 			tmp_boxes.push_back(p);
-			tmp_probabilities.push_back(output[i*7 + 2]);
-			tmp_classes.push_back(output[i*7 + 1]);
+			tmp_probabilities.push_back(output[i * 7 + 2]);
+			tmp_classes.push_back(output[i * 7 + 1]);
 		}
 	}
+
 	std::vector<unsigned> indices;
+
 	nms(tmp_boxes, tmp_probabilities, nmsThreshold, indices);
-	
-	boxes = std::vector<cv::Rect>(indices.size());
-	probabilities = std::vector<float>(indices.size());
-	classes = std::vector<unsigned>(indices.size());
+
+	boxes.resize(indices.size());
+	probabilities.resize(indices.size());
+	classes.resize(indices.size());
+
 	for (int i = 0; i < indices.size(); i++)
 	{
 		unsigned pos = indices[i];
-		boxes.push_back(tmp_boxes[pos]);
-		probabilities.push_back(tmp_probabilities[pos]);
-		classes.push_back(tmp_classes[pos]);
+		boxes[i]= tmp_boxes[pos];
+		probabilities[i]= tmp_probabilities[pos];
+		classes[i]=tmp_classes[pos];
 	}
 }
+
 
 
 void nms(const std::vector<cv::Rect>& boxes, const std::vector<float>& probabilities,
 	float threshold, std::vector<unsigned>& indices) 
 {
 	indices.resize(boxes.size());
+	
 	int size = boxes.size();
-	std::vector<unsigned> tmp_indices;
-	for (int i = 1; i < boxes.size(); ++i)
-		tmp_indices[i] = i;
 
-	for (int i = 0; i < size; i++)
-		for (int j = i + 1; j < size; j++)
+	for (int i = 0; i < size; i++) 
+		indices[i] = i;
+
+	for (int i = 0; i < size; i++) 
+	{
+		for (int j = i+1; j < size; j++) 
 		{
-			if (iou(boxes[i], boxes[j]) > threshold)
+			if (iou(boxes[i],boxes[j]) > threshold)
 			{
-				if (probabilities[i] > probabilities[j])
-				{
-					tmp_indices[j] = -1;
-
-				}
+				if (probabilities[i] > probabilities[j]) 
+					indices[j] = -1;
+			
 				else
 				{
-					tmp_indices[i] = -1;
+					indices[i] = -1;
 					break;
 				}
-
 			}
 		}
-	tmp_indices.erase(std::remove(tmp_indices.begin(), tmp_indices.end(), -1), tmp_indices.end());
-	for (int i = 1; i < indices.size(); ++i)
-		indices.push_back(tmp_indices[i]);
+		for (int k = i; k > 0; --k) 
+		{
+			if (indices[k] != -1 && indices[k - 1] != -1 && probabilities[indices[k]] > probabilities[indices[k - 1]]) 
+			{
+				unsigned pos=indices[k];
+				indices[k] = indices[k - 1];
+				indices[k - 1] = pos;
+			}
+			else 
+				break;
+		}
+	}
+	indices.erase(std::remove(indices.begin(), indices.end(), -1), indices.end());
 }
+
 float iou(const cv::Rect& a, const cv::Rect& b) {
 
 	return static_cast<float>((a & b).area()) / (a.area() + b.area() - (a & b).area());
