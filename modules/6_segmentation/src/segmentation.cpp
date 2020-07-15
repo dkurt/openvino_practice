f510845b90101b5be88adaa0fb2e4756ed12360f
Artem Mitrokhin <artemitrokhin@gmail.com>
2020-07-15 16:44:35 +0300

    dice and segment are working 4/8 tests

--- modules/6_segmentation/src/segmentation.cpp
+++ modules/6_segmentation/src/segmentation.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 using namespace cv::utils::fs;
 
 float Dice(const Mat& a, const Mat& b) {
-    CV_Error(Error::StsNotImplemented, "Dice score computation");
+    return 2.0f * countNonZero(a & b) / (countNonZero(a) + countNonZero(b));
 }
 
 ADAS::ADAS() {
@@ -16,8 +16,44 @@ ADAS::ADAS() {
     // Load deep learning network into memory
     auto net = ie.ReadNetwork(join(DATA_FOLDER, "semantic-segmentation-adas-0001.xml"),
                               join(DATA_FOLDER, "semantic-segmentation-adas-0001.bin"));
+
+    InputInfo::Ptr inputInfo = net.getInputsInfo()["data"];
+
+    inputInfo->getPreProcess().setResizeAlgorithm(ResizeAlgorithm::RESIZE_BILINEAR);
+    inputInfo->setLayout(Layout::NHWC);
+    inputInfo->setPrecision(Precision::U8);
+
+    // Initialize runnable object on CPU device
+    ExecutableNetwork execNet = ie.LoadNetwork(net, "CPU");
+        
+    // Create a single processing thread
+    req = execNet.CreateInferRequest();
+
+    outputName = net.getOutputsInfo().begin()->first;
+
+}
+
+static Blob::Ptr wrapMatToBlob(const Mat& m) {
+    CV_Assert(m.depth() == CV_8U);
+    std::vector<size_t> dims = { 1, (size_t)m.channels(), (size_t)m.rows, (size_t)m.cols };
+    return make_shared_blob<uint8_t>(TensorDesc(Precision::U8, dims, Layout::NHWC),
+        m.data);
 }
 
 void ADAS::segment(const Mat& image, Mat& mask) {
-    CV_Error(Error::StsNotImplemented, "ADAS semantic segmentation");
+    Blob::Ptr input = wrapMatToBlob(image);
+
+    req.SetBlob("data", input);
+
+    // Launch network
+    req.Infer();
+
+    // Copy output. 
+    int* output = req.GetBlob(outputName)->buffer();    
+    mask = Mat(req.GetBlob(outputName)->size(), 1, CV_32SC1, output);
+    mask = mask.reshape(1, 1024);
+    mask.convertTo(mask, CV_32F);
+    resize(mask, mask, Size(image.cols, image.rows));
+    mask.convertTo(mask, CV_8UC1);
+    std::cout << req.GetBlob(outputName)->size() << std::endl;
 }
