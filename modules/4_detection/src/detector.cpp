9ded1a2d43a04a50fae553a9a0be578271f33c9a
unknown <dim.trif1234@yandex.ru>
2020-07-17 15:48:25 +0300

    added detect

--- modules/4_detection/src/detector.cpp
+++ modules/4_detection/src/detector.cpp
@@ -9,20 +9,28 @@ using namespace InferenceEngine;
 
 Detector::Detector() {
     Core ie;
-
     // Load deep learning network into memory
     auto net = ie.ReadNetwork(utils::fs::join(DATA_FOLDER, "face-detection-0104.xml"),
-                              utils::fs::join(DATA_FOLDER, "face-detection-0104.bin"));
-    InputInfo::Ptr input_info = net.getInputsInfo().begin()->second;
-    std::string input_name = net.getInputsInfo().begin()->first;
-    
-    input_info->getPreProcess().setResizeAlgorithm(RESIZE_BILINEAR);
-    input_info->setLayout(Layout::NHWC);
-    input_info->setPrecision(Precision::U8);
+        utils::fs::join(DATA_FOLDER, "face-detection-0104.bin"));
 
+    InputInfo::Ptr inputInfo = net.getInputsInfo()["image"];
+    inputInfo->getPreProcess().setResizeAlgorithm(ResizeAlgorithm::RESIZE_BILINEAR);
+    inputInfo->setLayout(Layout::NHWC);
+    inputInfo->setPrecision(Precision::U8);
+    outputName = net.getOutputsInfo().begin()->first;
+
+    // Initialize runnable object on CPU device
     ExecutableNetwork execNet = ie.LoadNetwork(net, "CPU");
 
+    // Create a single processing thread
+    req = execNet.CreateInferRequest();
+}
 
+Blob::Ptr wrapMatToBlob(const Mat& m) {
+    CV_Assert(m.depth() == CV_8U);
+    std::vector<size_t> dims = { 1, (size_t)m.channels(), (size_t)m.rows, (size_t)m.cols };
+    return make_shared_blob<uint8_t>(TensorDesc(Precision::U8, dims, Layout::NHWC),
+        m.data);
 }
 
 void Detector::detect(const cv::Mat& image,
@@ -31,12 +39,56 @@ void Detector::detect(const cv::Mat& image,
                       std::vector<cv::Rect>& boxes,
                       std::vector<float>& probabilities,
                       std::vector<unsigned>& classes) {
+    Blob::Ptr input = wrapMatToBlob(image);
+    req.SetBlob("image", input);
 
+    // Launch network
+    req.Infer();
 
+    int x_min, y_min, x_max, y_max;
 
-    //CV_Error(Error::StsNotImplemented, "detect");
+    std::vector<Rect> boxesCopy;
+    std::vector<float> probabilitiesCopy;
+    std::vector<unsigned> classesCopy;
+
+    float* output = req.GetBlob(outputName)->buffer();
+    int size = req.GetBlob(outputName)->size() / 7;
+    
+    for (int i = 0; i < size; ++i) {
+
+        float probability = output[i * 7  + 2];
+        
+        if (probability > probThreshold) {
+            
+            int height = image.rows;
+            int width = image.cols;
+
+            x_min = output[i * 7 + 3] * width;
+            y_min = output[i * 7 + 4] * height;
+            x_max = output[i * 7 + 5] * width;
+            y_max = output[i * 7 + 6] * height;
+
+            Rect approvedRect(x_min, y_min, int(x_max - x_min + 1), int(y_max - y_min + 1));
+            boxesCopy.push_back(approvedRect);
+
+            unsigned classIndex = output[i * 7 + 1];
+            classesCopy.push_back(classIndex);
+            classes.push_back(classIndex);
+
+            probabilitiesCopy.push_back(probability);
+        }
+    }
+
+    std::vector<unsigned> indices;
+    nms(boxesCopy, probabilitiesCopy, nmsThreshold, indices);
+
+    for (auto indice : indices)
+    {
+        boxes.push_back(boxesCopy[indice]);
+        classes.push_back(classesCopy[indice]);
+        probabilities.push_back(probabilitiesCopy[indice]);
+    }
 }
-
 
 void nms(const std::vector<cv::Rect>& boxes, const std::vector<float>& probabilities,
          float threshold, std::vector<unsigned>& indices) {
